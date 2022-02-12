1354c2d586ce009456bdff6c6c954d5fc518d36b
dmoradiya <dharmesh@ualberta.ca>
2022-02-12 13:41:06 -0700

    add cart path in myshop/urls.py

--- myshop/myshop/urls.py
+++ myshop/myshop/urls.py
@@ -19,7 +19,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),   
+    path('cart/', include('cart.urls', namespace='cart')),
     path('', include('shop.urls', namespace='shop')),
 ]
 if settings.DEBUG:
