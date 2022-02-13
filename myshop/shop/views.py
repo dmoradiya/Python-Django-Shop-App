838c5010b217c3024d465d500f852076bd1aaf9b
dmoradiya <dharmesh@ualberta.ca>
2022-02-12 17:00:31 -0700

    add pagination in product list template

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -1,20 +1,30 @@
+import imp
+import re
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 from cart.forms import CartAddProductForm
+from django.core.paginator import Paginator
 
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available = True)
+
+
     if category_slug:
         category = get_object_or_404(Category, slug=(category_slug))
         products = products.filter(category = category)
+
+    paginator = Paginator(products, 2)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
     return render(request, 
                     'shop/product/list.html',
                     {
                         'category' : category,
                         'categories' : categories,
-                        'products' : products   
+                        'products' : page_obj   
                     })
 
 
