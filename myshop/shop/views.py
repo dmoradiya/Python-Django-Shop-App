bbe7ec7c84500cdeab6b85d1762ec68ecb55966d
dmoradiya <dharmesh@ualberta.ca>
2022-02-21 14:00:42 -0700

    create cart session to store cart quantity for list view add to cart

--- myshop/shop/views.py
+++ myshop/shop/views.py
@@ -3,6 +3,8 @@ from .models import Category, Product
 from cart.forms import CartAddProductForm
 from django.core.paginator import Paginator
 from django.views import View
+from django.conf import settings
+
 
 # List View Page
 class Product_List(View):
@@ -31,19 +33,39 @@ class Product_List(View):
                         })
 
     def post(self, request):
-        productid = request.POST.get('product')
+        # Get ProductID from add to cart form
+        productId = request.POST.get('productId')
+
+        cart = request.session.get(settings.CART_SESSION_ID)
+        print('first cart obj : ',cart)
+        
+        if cart:
+            quantity = cart.get(productId)
+            print(quantity)
+            if quantity:
+                cart[productId] = quantity + 1
+            else:
+                cart[productId] = 1
+        else:
+            cart = {}
+
+        request.session['settings.CART_SESSION_ID'] = cart
+
+        print('cart :', request.session['settings.CART_SESSION_ID'])
         
         return redirect('/')
 
 # Detail View Page
 def product_detail(request, id, slug):
+
+    # request.session.get(settings.CART_SESSION_ID).clear()
+
     product = get_object_or_404(Product, 
                                 id=id,
                                 slug=slug,
                                 available=True)
 
-    add_to_cart_product_form = CartAddProductForm()
+    # add_to_cart_product_form = CartAddProductForm()
     return render(request, 
                 'shop/product/detail.html',
-                {'product': product,
-                'add_to_cart_product_form': add_to_cart_product_form})
+                {'product': product})
